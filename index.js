9895caccf8fa82a253e22f4200e1813570bc8f27
bLuDd1 <dtavluj@gmail.com>
2023-05-24 15:30:43 +0300

    added comments to built-in tests

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class List {
     }
 
     length() {
-        this.array.length;
+        return this.array.length;
     }
 
     append(element) {
@@ -30,6 +30,9 @@ class List {
     }
 
     get(index) {
+        if (this.array[index] === undefined) {
+            console.log('Must be correct index!');
+        }
         return this.array[index];
     }
 
@@ -40,23 +43,66 @@ class List {
     }
 
     reverse() {
-        this.array.reverse();
+        return this.array.reverse();
     }
 
     findFirst(element) {
-        this.array.indexOf(element);
+        return this.array.indexOf(element);
     }
 
     findLast(element) {
-        this.array.lastIndexOf(element);
+        return this.array.lastIndexOf(element);
     }
 
     clear() {
-        this.array = [];
+        return this.array = [];
     }
 
     extend(elements) {
-        elements.array.map((el) => this.array.push(el));
+        elements.array.map((el) => this.append(el));
     }
-
 }
+
+const list = new List();
+
+list.append(1)
+console.log(list.array); // [ 1 ]
+
+list.insert(2, 0);
+list.insert(5, 1);
+console.log(list.array); // [ 2, 5, 1 ]
+
+console.log(list.length()); // 3
+
+console.log(list.reverse()); // [ 1, 5, 3 ]
+
+list.insert(5, 3);
+console.log(list.array); // [ 1, 5, 2, 5 ]
+
+console.log(list.findFirst(5)); // 1
+
+console.log(list.findLast(5)); // 3
+
+const clone = list.clone();
+console.log(clone.array); // [ 1, 5, 2, 5 ]
+
+console.log(clone.clear()); // []
+
+list.delete(0);
+console.log(list.array); // [ 5, 2, 5 ]
+
+list.deleteAll(5);
+console.log(list.array); // [ 2 ]
+
+list.append(7);
+list.append(7);
+list.append(7);
+console.log(list.array); // [ 2, 7, 7, 7 ]
+
+console.log(list.get(0)); // 2
+
+console.log(list.array); // [ 2, 7, 7, 7 ]
+
+clone.append(3);
+list.extend(clone);
+console.log(list.array); // [ 2, 7, 7, 7, 3 ]
